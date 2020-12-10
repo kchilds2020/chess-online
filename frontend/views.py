67041e3a6ee12e1670e103ab5e682e4de8c08267
Kevin Desktop <kchilds2020@gmail.com>
2020-12-10 16:01:07 -0600

    initial commit

--- frontend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request, *args, **kwargs):
-    """
-    docstring
-    """
-    return render(request, 'frontend/index.html')
